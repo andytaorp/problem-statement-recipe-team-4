a996ddd9ad801cd296651ce0a131b12860962460
Xire23 <23025337@myrp.edu.sg>
2025-02-04 10:48:16 +0800

    r

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import { useAuthContext, AuthContextProvider } from './hooks/useAuthContext'; // Import AuthContextProvider
+import { useAuthContext } from './hooks/useAuthContext';
 
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
@@ -11,27 +11,25 @@ function App() {
 
   return(
     <div className="App">
-      <AuthContextProvider> {/* Wrap BrowserRouter with AuthContextProvider */}
-        <BrowserRouter>
-          <Navbar/>
-          <div className='pages'>
-            <Routes>
-              <Route 
-                path='/' 
-                element={user ? <Home/> : <Navigate to="/login"/>}
-              />
-              <Route 
-                path='/login' 
-                element={!user ? <Login/> : <Navigate to="/"/>}
-              />
-              <Route 
-                path='/signup' 
-                element={!user ? <Signup/> : <Navigate to="/"/>}
-              />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </AuthContextProvider>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='pages'>
+          <Routes>
+            <Route 
+              path='/' 
+              element={user ? <Home/> : <Navigate to="/login"/>}
+            />
+            <Route 
+              path='/login' 
+              element={!user ? <Login/> : <Navigate to="/"/>}
+            />
+            <Route 
+              path='/signup' 
+              element={!user ? <Signup/> : <Navigate to="/"/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
