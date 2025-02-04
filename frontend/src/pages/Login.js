4eff01d8a8d2ac29038571fcd2161057ad0674e5
Xire23 <23025337@myrp.edu.sg>
2025-02-04 10:02:19 +0800

    r

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,33 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {login, error, isLoading} = useLogin();
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(email, password)
-    }
+        e.preventDefault();
+
+        await login(email, password);
+    };
 
     return (
-        <form className="signup" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
-
+        <form className="login" onSubmit={handleSubmit}>
+            <h3>Log In</h3>
             <label>Email:</label>
-            <input
+            <input 
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
             />
-
             <label>Password:</label>
-            <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
+            <input 
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
             />
-
-            <button>Log in</button>
+            <button disabled={isLoading}>Log In</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,32 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {signup, error, isLoading} = useSignup();
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-    }
+        e.preventDefault();
+        
+        await signup(email, password);
+    };
 
     return (
         <form className="signup" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
-
+            <h3>Sign up</h3>
             <label>Email:</label>
-            <input
+            <input 
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
             />
-
             <label>Password:</label>
-            <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
+            <input 
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
             />
-
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
